d64c2099ff4e2a3fc5d1e95839f804f4538f67cf
Diego Lopez Grisanti <diegolopezgrisanti@gmail.com>
2021-02-19 17:16:45 -0300

    Merge branch 'develop' of https://github.com/solblack/cook-app-api into feature/26-error-middleware

--- src/controllers/recipe.controller.js
+++ src/controllers/recipe.controller.js
@@ -41,6 +41,11 @@ class RecipeController {
           },
         ],
       });
+      if(!recipe){
+        const error = new Error('Bad request. Invalid ID');
+        error.status = 400;
+        throw error;
+      }
       res.status(200).json(recipe);
     } catch (err) {
       next(err);
@@ -78,6 +83,14 @@ class RecipeController {
   update = async (req, res) => {
     const transaction = await db.sequelize.transaction();
     try {
+      const recipe = await db.Recipe.findByPk(req.params.id);
+
+      if(!recipe){
+        const error = new Error('Bad request. Invalid ID');
+        error.status = 400;
+        throw error;
+      }
+
       await db.Recipe.update(
         {
           title: req.body.title,
@@ -92,12 +105,10 @@ class RecipeController {
         }
       );
 
-      const recipe = await db.Recipe.findByPk(req.params.id);
-
       await transaction.commit();
       res
         .status(200)
-        .json({ message: `product with id ${req.params.id} edited`, recipe });
+        .json({ message: `Recipe with id ${req.params.id} edited`, recipe });
     } catch (err) {
       await transaction.rollback();
       next(err);
@@ -111,16 +122,161 @@ class RecipeController {
    */
   delete = async (req, res, next) => {
     try {
+      const recipe = await db.Recipe.findByPk(req.params.id);
+
+      if(!recipe){
+        const error = new Error('Bad request. Invalid ID');
+        error.status = 400;
+        throw error;
+      }
+
       const deleteRecipe = await db.Recipe.destroy({
+        where: {
+          id: req.params.id,
+        },
+      });
+
+      res
+        .status(200)
+        .json({ message: `Recipe with id ${req.params.id} deleted` });
+    } catch (err) {
+      next(err);
+    }
+  };
+
+  /**
+   * Add ingredient to recipe
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  addIngredient = async (req, res, next) => {
+    const transaction = await db.sequelize.transaction();
+    try {
+      // Validate IDs
+      const ingredient = await db.Ingredient.findByPk(req.body.ingredient_id);
+      if (!ingredient) {
+        const error = new Error("Bad request. Invalid ingredient ID");
+        error.status = 400;
+        throw error;
+      }
+      const recipe = await db.Recipe.findByPk(req.params.recipeId);
+      if (!recipe) {
+        const error = new Error("Bad request. Invalid recipe ID");
+        error.status = 400;
+        throw error;
+      }
+      const unitMeasure = await db.UnitMeasure.findByPk(
+        req.body.unit_measure_id
+      );
+      if (!unitMeasure) {
+        const error = new Error("Bad request. Invalid unit measure ID");
+        error.status = 400;
+        throw error;
+      }
+
+      let recipeIngredient = {
+        recipe_id: req.params.recipeId,
+        ingredient_id: req.body.ingredient_id,
+        quantity: req.body.quantity,
+        unit_measure_id: req.body.unit_measure_id,
+      };
+
+      const newRecipeIngredient = await db.RecipeIngredient.create(
+        recipeIngredient,
+        { transaction }
+      );
+      await transaction.commit();
+      res.status(200).json(newRecipeIngredient);
+    } catch (error) {
+      await transaction.rollback();
+      res
+        .status(error.status || 500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  };
+
+  /**
+   * Edit ingredient to recipe
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  editIngredient = async (req, res, next) => {
+    const transaction = await db.sequelize.transaction();
+    try {
+      // Validate IDs
+      const unitMeasure = await db.UnitMeasure.findByPk(
+        req.body.unit_measure_id
+      );
+      if (!unitMeasure) {
+        const error = new Error("Bad request. Invalid unit measure ID");
+        error.status = 400;
+        throw error;
+      }
+      const recipeIngredientExists = await db.RecipeIngredient.findByPk(
+        req.params.id
+      );
+      if (!recipeIngredientExists) {
+        const error = new Error("Bad request. Invalid recipe ingredient ID");
+        error.status = 400;
+        throw error;
+      }
+
+      let recipeIngredient = {
+        quantity: req.body.quantity,
+        unit_measure_id: req.body.unit_measure_id,
+      };
+
+      await db.RecipeIngredient.update(recipeIngredient, {
+        transaction,
+        where: { id: req.params.id },
+      });
+      await transaction.commit();
+      res
+        .status(200)
+        .json({
+          message: `Recipe ingredient with id ${req.params.recipeId} edited`,
+        });
+    } catch (error) {
+      await transaction.rollback();
+      res
+        .status(error.status || 500)
+        .json({ message: error.message || "Internal server error" });
+    }
+  };
+
+  /**
+   * Delete ingredient from recipe
+   * @param {*} req
+   * @param {*} res
+   * @param {*} next
+   */
+  deleteIngredient = async (req, res, next) => {
+    try {
+      // Validate IDs
+      const recipeIngredient = await db.RecipeIngredient.findByPk(
+        req.params.id
+      );
+      if (!recipeIngredient) {
+        const error = new Error("Bad request. Invalid recipe ingredient ID");
+        error.status = 400;
+        throw error;
+      }
+       await db.RecipeIngredient.destroy({
         where: {
           id: req.params.id,
         },
       });
       res
         .status(200)
-        .json({ message: `product with id ${req.params.id} deleted` });
-    } catch (err) {
-      next(err);
+        .json({
+          message: `Recipe ingredient with id ${req.params.recipeId} deleted`,
+        });
+    } catch (error) {
+      res
+        .status(error.status || 500)
+        .json({ message: error.message || "Internal server error" });
     }
   };
 }
--- src/routes/recipe.routes.js
+++ src/routes/recipe.routes.js
@@ -8,5 +8,8 @@ router.get("/:id", recipeController.getOneById);
 router.post("/", recipeController.create);
 router.put("/:id", recipeController.update);
 router.delete("/:id", recipeController.delete);
+router.post("/:recipeId/ingredients", recipeController.addIngredient);
+router.put("/:recipeId/ingredients/:id", recipeController.editIngredient);
+router.delete("/:recipeId/ingredients/:id", recipeController.deleteIngredient);
 
 module.exports = router;
